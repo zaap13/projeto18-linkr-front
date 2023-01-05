b139be1e10fdb583f64cd08651fe76e4adb9b904
Josana-Kla <josana.0205@gmail.com>
2023-01-05 10:09:22 -0300

    css: initialized header component css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
-import HeaderStyle from "./HeaderStyle";
+import { HeaderStyle, SearchContainer } from "./HeaderStyle";
 
 export default function Header() {
     return(
         <HeaderStyle>
             <h1>linkr</h1>
+            <SearchContainer>
+                <div>
+                    <input type="text" placeholder="Search for people"/>
+                    <button type="submit">
+                        <ion-icon name="search"></ion-icon>
+                    </button>
+                </div>
+                {/* <div>
+                    <img src="https://a-static.mlcdn.com.br/800x560/carrinho-brinquedo-vermelho-racer-55-mk206-dismat-dismat-brinquedos/emporiocasa/921943/988c845844f9d8c11c4b14cc7aaff15c.jpg" alt="profile"/>
+                    <p>Nome da pessoa</p>
+                </div> */}
+            </SearchContainer>
             <div>
-                <input type="text" placeholder="Search for people"/>
-                <button type="submit">
-                    <ion-icon name="search"></ion-icon>
-                </button>
-            </div>
-            <div>
-                <ion-icon name="chevron-down"></ion-icon>
-                <img src="" alt="profile"/>
+                <a href="*">
+                    <ion-icon name="chevron-down"></ion-icon>
+                    <ion-icon name="chevron-up">
+                        <a href="*">Logout</a>
+                    </ion-icon>
+                </a>
+                <img src="https://a-static.mlcdn.com.br/800x560/carrinho-brinquedo-vermelho-racer-55-mk206-dismat-dismat-brinquedos/emporiocasa/921943/988c845844f9d8c11c4b14cc7aaff15c.jpg" alt="profile"/>
             </div>
         </HeaderStyle>
     )
--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -1,22 +1,47 @@
 import styled from "styled-components";
 
-const HeaderStyle = styled.header`
-    background-color: #126BA5;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
-    padding: 0 18px;
+const HeaderStyle = styled.nav`
+    background-color: #151515;
+    padding: 0 15px;
+
     width: 100%;
-    height: 70px;
+    height: 72px;
+
     display: flex;
     align-items: center;
     justify-content: space-between;
+
     position: fixed;
     top: 0;
     left: 0;
+
+    h1 {
+        font-size: 49px;
+        font-weight: bold;
+        letter-spacing: 5%;
+        color: #FFFFFF;
+    }
+
     img:nth-child(2) {
         width: 51px;
         height: 51px;
+
         border-radius: 50%;
     }
-` 
+`; 
 
-export default HeaderStyle;
+const SearchContainer = styled.div`
+    background-color: #FFFFFF;
+    border-radius: 8px;
+    padding-left: 15px;
+
+    width: 563px;
+    height: 45px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+
+export { HeaderStyle, SearchContainer };
