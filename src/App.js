abbb020c65700c160748023f15f22e2fe68b1d3e
Dayane <acdayane@gmail.com>
2023-01-06 17:17:39 -0300

    bugs(userpage)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import GlobalStyle from "./assets/styles/globalStyles";
 import { BrowserRouter } from "react-router-dom";
 import AuthContext from "./contexts/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import PrivateRoutes from "./routes/private.routes";
 import PublicRoutes from "./routes/public.routes";
 
 function App() {
   const { auth, setAuth } = useContext(AuthContext);
-  const getToken = JSON.parse(localStorage.getItem("linkr"));
-  
-  if(getToken) {
-    setAuth(true);
-  };
+  console.log(auth)
+
+  useEffect(() => {
+    const getToken = JSON.parse(localStorage.getItem("linkr"));
+    console.log(getToken)
+    if (getToken) {
+      setAuth(true);
+    };
+  }, [auth]);
+
 
   return (
     <BrowserRouter>
       <GlobalStyle />
+      {/* <PrivateRoutes /> */}
       {auth ? <PrivateRoutes /> : <PublicRoutes />}
     </BrowserRouter>
   );
