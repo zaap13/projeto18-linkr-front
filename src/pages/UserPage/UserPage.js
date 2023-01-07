b2760c255c7d877983f2f7bd51eb95c2d69d30d1
Dayane <acdayane@gmail.com>
2023-01-06 21:28:00 -0300

    fix(auth problem - show all routes)

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL, loading_url } from "../../constants/url";
 import { PublicMain } from "../../assets/styles/styles";
@@ -8,6 +9,7 @@ import { ContainerPosts, Gif } from "./UserPageStyle"
 
 
 export default function UserPage() {
+    //const { id } = useParams();
     const id = 10
     const [userData, setUserData] = useState("");
     const [userPosts, setUserPosts] = useState("");
@@ -20,9 +22,9 @@ export default function UserPage() {
                 Authorization: `Bearer ${getToken.token}`,
             },
         };
-
+console.log(`${BASE_URL}/user/${id}`, config)
         axios
-            .get(`${BASE_URL}/user/:${id}`, config)
+            .get(`${BASE_URL}/user/${id}`, config)
             .then((res) => {
                 setUserData(res.data);
                 setLoading(false);
@@ -32,7 +34,7 @@ export default function UserPage() {
             });
     }, []);
 
-    console.log(userData)
+console.log(userData)
 
     if (loading === true) {
         return (
@@ -49,7 +51,7 @@ export default function UserPage() {
         <PublicMain>
             <Header />
             <ContainerPosts>
-                
+                <h1>{userData.username}</h1>
                 <PostCard />
                 <PostCard />
                 <PostCard />
