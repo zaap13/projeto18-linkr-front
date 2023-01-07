849ef76acfe8cf90666ed9e101f6fcbe51f20218
Pedro Rondelli <pedrorondelli@hotmail.com>
2023-01-07 15:52:16 -0300

    feat: install metadata-scraper and creat getUrlData wich one execute getMetaData

--- src/pages/TimeLine/Post.js
+++ src/pages/TimeLine/Post.js
@@ -1,12 +1,19 @@
 import { PostCard } from "../../assets/styles/styles";
+import getMetaData from "metadata-scraper";
+
+async function getUrlData(link) {
+  const url = link;
+  const data = await getMetaData(url);
+  return data;
+}
 
 export default function Post({ name, picture, content, url }) {
+  const urlDetails = getUrlData(url);
   return (
     <PostCard>
       <img src={picture} alt="profile"></img>
       <h1>{name}</h1>
       <p>{content}</p>
-      <a href={url}>{url}</a>
     </PostCard>
   );
 }
