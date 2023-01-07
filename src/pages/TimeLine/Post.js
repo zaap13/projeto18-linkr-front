5f79cca0f0d928aacb8933a170dfae8c457ac353
Pedro Rondelli <pedrorondelli@hotmail.com>
2023-01-07 16:21:12 -0300

    fix: fixes bug into Postcard

--- src/pages/TimeLine/Post.js
+++ src/pages/TimeLine/Post.js
@@ -1,14 +1,6 @@
 import { PostCard } from "../../assets/styles/styles";
-import getMetaData from "metadata-scraper";
-
-async function getUrlData(link) {
-  const url = link;
-  const data = await getMetaData(url);
-  return data;
-}
 
 export default function Post({ name, picture, content, url }) {
-  const urlDetails = getUrlData(url);
   return (
     <PostCard>
       <img src={picture} alt="profile"></img>
